8af2871e7334e4b27283aa269cb19c2f6e0badda
Vaid Sjothonov <mode_private@mail.ru>
2023-10-19 00:01:22 +0400

    added fragments, bottom_navigation_menu.xml and styles, colors

--- app/src/main/java/projects/school/comunication/ui/activities/MainActivity.kt
+++ app/src/main/java/projects/school/comunication/ui/activities/MainActivity.kt
@@ -1,11 +1,13 @@
-package projects.school.comunication
+package projects.school.comunication.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import projects.school.comunication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
