63f3dbc9ef6d5a1ed62070e897449a74ba796084
Vaid Sjothonov <mode_private@mail.ru>
2023-10-19 14:15:23 +0400

    created the navigation graph and bound it with bottomNavigationView

--- app/src/main/java/projects/school/comunication/ui/activities/MainActivity.kt
+++ app/src/main/java/projects/school/comunication/ui/activities/MainActivity.kt
@@ -2,12 +2,20 @@ package projects.school.comunication.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import projects.school.comunication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //bind navController and BottomNavigationView
+        val bottomNavigationView:BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navigationController = findNavController(R.id.fragmentContainerView)
+        bottomNavigationView.setupWithNavController(navigationController)
 
     }
 }
